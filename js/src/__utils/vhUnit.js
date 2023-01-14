d1cf25407bf04b97bb51732a95f2fab593cc66f6
yeroslav.k <yeroslav.k@appsflyer.com>
2023-01-14 14:03:20 +0200

    Clean up

--- js/src/__utils/vhUnit.js
+++
@@ -1,8 +0,0 @@
-const vhUnit = () => {
-	document.documentElement.style.setProperty(
-		'--vh',
-		`${window.innerHeight * 0.01}px`
-	);
-};
-
-export default vhUnit;
--- js/src/editor-formats/icon-list/script.js
+++
@@ -1,7 +0,0 @@
-const { wp } = window;
-const { registerBlockStyle } = wp.blocks;
-
-registerBlockStyle('core/list', {
-	name: 'check-icon',
-	label: 'Check Icon',
-});
