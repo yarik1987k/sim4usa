b7bffd3b7dfa3df6d6199a5d6605bdb9e29a89d9
yeroslav.k <yeroslav.k@appsflyer.com>
2023-01-19 18:07:27 +0200

    Test run pipline

--- includes/webhook.php
+++ includes/webhook.php
@@ -23,28 +23,4 @@
   *    (e.g., http://example.com/webhook.php)
   *
 **/
-
-// Set Variables
-$LOCAL_ROOT         = get_template_directory().'/test';
-$LOCAL_REPO_NAME    = "test";
-$LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
-$REMOTE_REPO        = "git@github.com:yarik1987k/sim4usa.git";
-$BRANCH             = "main";
  
-if ( isset($_POST['payload'] )) {
-  // Only respond to POST requests from Github
- 
-  if( file_exists($LOCAL_REPO) ) {
-    
-    // If there is already a repo, just run a git pull to grab the latest changes
-    shell_exec("cd {$LOCAL_REPO} && git pull");
-
-    die("done " . mktime());
-  } else {
-    
-    // If the repo does not exist, then clone it into the parent directory
-    shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO}");
-    
-    die("done " . mktime());
-  }
-}
