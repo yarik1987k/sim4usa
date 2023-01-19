5e57c639c95d05cb32833bf6eea1ad3e8d4c4ec7
yeroslav.k <yeroslav.k@appsflyer.com>
2023-01-19 21:32:05 +0200

    disable zoom options on product page

--- includes/woo-functions/mini-cart.php
+++ includes/woo-functions/mini-cart.php
@@ -4,3 +4,5 @@ function small_cart(){
     $small_cart = '<button class="mini-cart-button"><span class="mini-cart-button__count-price">$0.00</span><span class="icon-cart"></span></button>';
     return $small_cart;
 }
+
+add_filter( 'woocommerce_single_product_zoom_enabled', '__return_false' );
