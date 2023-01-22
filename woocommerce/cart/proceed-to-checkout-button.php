306f7baba095efe1305bd16cab58bd52f0324cf8
yeroslav.k <yeroslav.k@appsflyer.com>
2023-01-22 20:44:38 +0200

    test

--- woocommerce/cart/proceed-to-checkout-button.php
+++ woocommerce/cart/proceed-to-checkout-button.php
@@ -23,6 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <?php 
 	print_r(wc_get_checkout_url());
+	var_dump(wc_get_checkout_url());
 ?>
 <a href="<?php echo esc_url( wc_get_checkout_url() ); ?>" class="checkout-button button alt wc-forward<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>">
 	<?php esc_html_e( 'Proceed to checkout', 'woocommerce' ); ?>
