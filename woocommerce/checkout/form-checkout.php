ff033df6c1577737d835109976a11fe2385f9db8
yeroslav.k <yeroslav.k@appsflyer.com>
2023-01-22 21:06:59 +0200

    checkout page

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -18,7 +18,7 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
+echo '<div class="container"><div class="row"><div class="col-md-12">';
 do_action( 'woocommerce_before_checkout_form', $checkout );
 
 // If checkout registration is disabled and not logged in, the user cannot checkout.
@@ -65,3 +65,6 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 </form>
 
 <?php do_action( 'woocommerce_after_checkout_form', $checkout ); ?>
+	</div>
+	</div>
+	</div>
