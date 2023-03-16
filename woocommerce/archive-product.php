c28bca5e154738190a56aef3dcc8f7b3e182ef98
yeroslav.k <yeroslav.k@appsflyer.com>
2023-03-16 21:16:58 +0200

    Fixing archive

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -26,9 +26,13 @@ get_header( 'shop' );
  * @hooked woocommerce_breadcrumb - 20
  * @hooked WC_Structured_Data::generate_website_data() - 30
  */
-do_action( 'woocommerce_before_main_content' );
+
 
 ?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<?php do_action( 'woocommerce_before_main_content' );?>
 <header class="woocommerce-products-header">
 	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
@@ -54,7 +58,7 @@ if ( woocommerce_product_loop() ) {
 	 * @hooked woocommerce_result_count - 20
 	 * @hooked woocommerce_catalog_ordering - 30
 	 */
-	do_action( 'woocommerce_before_shop_loop' );
+	//do_action( 'woocommerce_before_shop_loop' );
 
 	woocommerce_product_loop_start();
 
@@ -100,6 +104,6 @@ do_action( 'woocommerce_after_main_content' );
  *
  * @hooked woocommerce_get_sidebar - 10
  */
-do_action( 'woocommerce_sidebar' );
-
+//do_action( 'woocommerce_sidebar' );
+echo '</div></div></div>';
 get_footer( 'shop' );
