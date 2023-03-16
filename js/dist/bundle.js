258e3421a5ff538992c53e96fb360d3eec856004
yeroslav.k <yeroslav.k@appsflyer.com>
2023-03-16 20:40:48 +0200

    Fixin errors

--- js/dist/bundle.js
+++ js/dist/bundle.js
@@ -1370,32 +1370,35 @@ class ImeiCheck {
 	  var submitImei = document.getElementById("submit-imei");
 	  var searchBox = document.getElementById("search-box-imei");
 	  var results = document.getElementById("result-imei");
-	  submitImei.addEventListener("click", function(e) {
-		  e.preventDefault();
-		// Get the search term from the input field
-		var searchTerm = searchBox.value;
-
-
-		 
-		$.ajax({
-			type: 'POST',
-			url: woocommerce_params.ajax_url,
-			dataType: 'JSON',
-			data: {
-				action: 'check_imei',
-				searchTerm: searchTerm
-			},
-			success(response) {
-				let json = JSON.parse(response);
-				if(json.status === "succes"){
-					results.innerHTML += 'Status: '+json.status+'<br>';
-					results.innerHTML += json.result;
-
-				}
-				//location.reload();
-			},
-		});  
-	  });
+	  if(submitImei){
+		submitImei.addEventListener("click", function(e) {
+			e.preventDefault();
+		  // Get the search term from the input field
+		  var searchTerm = searchBox.value;
+  
+  
+		   
+		  $.ajax({
+			  type: 'POST',
+			  url: woocommerce_params.ajax_url,
+			  dataType: 'JSON',
+			  data: {
+				  action: 'check_imei',
+				  searchTerm: searchTerm
+			  },
+			  success(response) {
+				  let json = JSON.parse(response);
+				  if(json.status === "succes"){
+					  results.innerHTML += 'Status: '+json.status+'<br>';
+					  results.innerHTML += json.result;
+  
+				  }
+				  //location.reload();
+			  },
+		  });  
+		});
+	  }
+	  
 	}
 
  
